9debc76f1bf3015ee6e43bbebda0f5254c034eda
mcamara75 <mcamara75@yahoo.es>
2021-03-19 19:32:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dmfa5m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dmfa5m)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { CartService } from "../cart.service";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: "",
+    address: ""
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSummit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn("Your order ha been summitted", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+  ngOnInit() {}
+}
